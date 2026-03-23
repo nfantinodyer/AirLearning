45e50fecccd019aeed6a4e02b07ed340fc804188
Nicholas Fantino-Dyer <2013RECK@gmail.com>
2026-03-23 13:13:32 -0700

    linked the text cleaner class to connect and used it for the functions.

--- Project1/connect.py
+++ Project1/connect.py
@@ -1,17 +1,27 @@
 from twoFunctions import FileReader,IdExtraction,EmailExtraction,WordFrequencyCounter
+from textCleaner import TextCleaner
 
 if __name__ == "__main__":
 
     location = "Project1/sample.txt"
     fileContents = FileReader(location)
-    
+
+    fileContents = TextCleaner.remove_extra_spaces(fileContents)
+    fileContents = TextCleaner.remove_special_characters(fileContents)
+    fileContents = TextCleaner.to_lowercase(fileContents)
+
     emails = EmailExtraction(fileContents)
+    print("Emails Extracted:")
     for i in emails:
         print(i)
+    print()
 
-    ids = IdExtraction(fileContents)
+    ids = IdExtraction(fileContents.upper()) #had to make it upper since the function needs to have AAA not aaa.
+    print("IDs:")
     for i in ids:
         print(i)
+    print()
 
     wordCount = WordFrequencyCounter(fileContents)
+    print("Word Count Dictionary:")
     print(wordCount)
